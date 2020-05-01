b8a5d32221af6af8cad414d406117b99a634361c
Kevin Ford <4380198+Bowman74@users.noreply.github.com>
2020-05-01 11:50:29 -0700

    Finish week 5 MML content

---
+++ Collateral/TensorFlowJS/wwwroot/js/tensorflowjs.js
@@ -0,0 +1,15 @@
+﻿async function predictDeaths() {
+    var cases = document.getElementById("noCases").value;
+
+    document.getElementById("noDeaths").innerText = await GetPrediction(cases);
+}
+
+async function GetPrediction(cases) {
+    const model = await tf.loadLayersModel("https://localhost:5001/tfjs/model.json");
+
+    const inputTensor = tf.tensor([[(cases / 100000) ** 3]]);
+
+    var result = model.predict(inputTensor);
+
+    return result.arraySync()[0][0];
+}
